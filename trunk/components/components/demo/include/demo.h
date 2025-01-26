4b7ea8c553eceb1fa4198837b60541079611a716
webspellernjwoc <weinengju@yeah.net>
2025-01-26 23:35:14 +0800

    git-svn-id: http://fsvr.apt-hz.com/svn/CODE_APT32F102x@42 45d71d15-3ab0-2042-9f20-2da16efb626a

--- trunk/components/components/demo/include/demo.h
+++ trunk/components/components/demo/include/demo.h
@@ -14,23 +14,25 @@
 #define _DEMO_H
 
 /* Includes ------------------------------------------------------------------*/
-//gpio port
+//gpio port demo
 extern int gpio_port_ouput_demo(void);
 extern int gpio_port_input_demo(void);
 extern int gpio_port_irq_demo(void);
 
-//pin 
+//pin demo
 extern int pin_output_demo(void);
 extern int pin_input_demo(void);
 extern int pin_irq_demo(void);
 
-//bt
+//bt demo
 extern int bt_timer_demo(void);
 extern int bt_pwm_demo(void);
 
-//iwdt
+//iwdt demo
 extern int iwdt_normal_demo(void);
 extern int iwdt_irq_demo(void);
 
+//etcb demo
+int etcb_one_trg_one_demo(void);
 
 #endif
