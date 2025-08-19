e451df6e91bbac3f96d7fe8eded2031c79cdf9b6
webspellernjwoc <weinengju@yeah.net>
2025-08-20 04:00:45 +0800

    git-svn-id: http://fsvr.apt-hz.com/svn/CODE_APT32F102x@78 45d71d15-3ab0-2042-9f20-2da16efb626a

--- trunk/board/src/board_config.c
+++ trunk/board/src/board_config.c
@@ -9,23 +9,34 @@
  * *********************************************************************
 */
 
-#include "sys_clk.h"
+#include <sys_clk.h>
 #include "board_config.h"
 #include "sys_console.h"
 
+/* Private macro------------------------------------------------------*/
+
+/* externs function---------------------------------------------------*/
 extern int32_t console_init(sys_console_t *handle);
+
+/* externs variablesr-------------------------------------------------*/
+
+/* variablesr---------------------------------------------------------*/
+sys_console_t console;
 
 /// system clock configuration parameters to define source, source freq(if selectable), sdiv and pdiv
 csi_clk_config_t tClkConfig = 
 	//{SRC_HFOSC, HFOSC_48M_VALUE, SCLK_DIV3, PCLK_DIV1, 5556000, 5556000};
 	//{SRC_EMOSC, 20000000, SCLK_DIV1, PCLK_DIV2, 5556000, 5556000};
-	{SRC_IMOSC, IMOSC_5M_VALUE, SCLK_DIV1, PCLK_DIV2,5556000, 5556000};
-	//{SRC_HFOSC, HFOSC_48M_VALUE, SCLK_DIV2, PCLK_DIV1,5556000, 5556000};
+	//{SRC_IMOSC, IMOSC_5M_VALUE, SCLK_DIV1, PCLK_DIV2,5556000, 5556000};
+	{SRC_HFOSC, HFOSC_48M_VALUE, SCLK_DIV2, PCLK_DIV1,5556000, 5556000};
 	//{SRC_IMOSC, IMOSC_4M_VALUE, SCLK_DIV1, PCLK_DIV1,5556000, 5556000};
 
 
-
-sys_console_t console;
+/** \brief board initialize config; 
+ * 
+ *  \param[in] none
+ *  \return none
+ */ 
 __attribute__((weak)) void board_init(void)
 {
     //init the console according to board_config.h
@@ -37,8 +48,7 @@ __attribute__((weak)) void board_init(void)
  
     console.rx.pin = CONSOLE_RXD;
     console.rx.func = CONSOLE_RXD_FUNC;
-    console.uart = NULL;
-
+	console.uart = (csp_uart_t *)(APB_UART0_BASE + CONSOLE_IDX * 0x1000);
     console_init(&console);
 
 }
